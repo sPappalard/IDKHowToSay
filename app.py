5febfe9dc5119a57bf419b09b7b2167976656d86
Salvatore Pappalardo <s.pappalardo@reply.it>
2025-09-21 22:58:16 +0200

    deploy

--- app.py
+++ app.py
@@ -626,4 +626,6 @@ def api_status():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug_mode = os.environ.get('FLASK_ENV') != 'production'
+    app.run(host='0.0.0.0', port=port, debug=debug_mode)
